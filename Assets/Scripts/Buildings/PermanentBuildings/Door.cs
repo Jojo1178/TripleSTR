da68485b7a1dcae5e451c4178c3f76fdff836da5
y4ka <loris.bezard@hotmail.fr>
2019-01-23 23:09:42 +0100

    New buildings organisation

---
+++ Assets/Scripts/Buildings/PermanentBuildings/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : PermanentBuilding, ISelectionableEntity
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.setName("Door");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Buildings/PermanentBuildings/Well.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Well : PermanentBuilding, ISelectionableEntity
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.setName("Well");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
